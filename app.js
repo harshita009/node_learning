c77e1aa15a4a6a584ecacea9b26b51b9b2fb7627
harshita <harshita.gupta@jungleworks.com>
2018-08-02 17:12:41 +0530

    email verification

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /**
  * Created by Harshita Gupta 20-07-2018
  */
+ require("dotenv").config();
  var express=require("express"),
  body_parser=require("body-parser"),
  register_user=require("./register_user")
@@ -21,15 +22,17 @@ app.get("/", (req, res) => {
 	res.send("Hello from Express!")//res.send uses content-type:text/html
 })
 
-
+app.post("/api/insert_user_details",register_user.insert_user_details);
 app.post("/api/generate_otp",register_user.generate_otp);
 app.post("/api/verify_otp",register_user.verify_otp);
+app.get("/api/generate_email",register_user.generate_email);
+app.get("/api/verify_email",register_user.verify_email);
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
     throw err
   }
-  console.log(`server is listening on ${process.env.PORT}`)
+  console.log(`server is listening on ${process.env.PORT}`);
 });
 app.post("/api/login",login_user.login);
 app.post("/api/securedapi",verify_token.verifytoken,secured_api.fun);
--- register_user.js
+++ register_user.js
@@ -2,9 +2,6 @@
 /**
  * Created by Harshita Gupta 20-07-2018
  */
-
-//Importing modules to be used. 
-
 var Cryptr=require("cryptr"),
 express=require("express"),
 connection=require("./db_connection").connection_obj,
@@ -14,178 +11,250 @@ SendOtp = require('sendotp'),
 randomize=require('randomatic'),
 sendOtp = new SendOtp(process.env.OTPKEY),
 cryptr = new Cryptr(process.env.SECRET),
-constants=require("./constants.js");
+boom=require("boom"),
+constants=require("./constants.js")
+nodemailer=require("nodemailer");
 
 
-/*
-There were two ways to do otp verification:
-1-Generate the otp 
-2-Store the otp in the db
-3-If the otp entered by the user matched the otp in the db for that user then user's phone number is verified.
-4-Otp is valid or invalid this will be verified using a mysql query.
-
-The second method is using the sendotp modules builtin functions for the purpose of otp verification
-and validity.
-
-Below is the implementation using second method.
-*/
+var smtpTransport =nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: process.env.EMAIL_ID,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
 
 
-//Generating the OTP.
-module.exports.generate_otp=function(req,res){
-	//encrypting the password bare password should not be stored in the db.
+
+
+
+
+
+module.exports.insert_user_details=function(req,res){
 	var encrptedPass=cryptr.encrypt(req.body.password);
-	
-
 	var user={
 		name:req.body.user_name,
 		password:encrptedPass,
 		email:req.body.email,
 		phone:req.body.phone,
 	};	
-	
-
-	/*Using aync.auto for the purpose of serializing the execution of sub tasks.
-    In async.auto the result is the argument passed with the callback function.
-    And every function passes the result till that function to the next function.
-    Important point to note is that if a function is dependent on some other function
-    the order in which the parameters are received is opposite to what it is in normal case.
-    i.e. The first argument is the result and the second one is the callback.
-    */
-    async.auto({ 
-
-    	//First function which will be executed it not dependent on any function.
-      	one : function(cb){
-      		/*Inserting all the details of the user in db.
-      		The values of the columns like otp_verified will be set to 0 by default.*/
-      		
-      		connection.query('INSERT INTO users SET ?;',user,function(err,results)
-      		{
-				if(err){
-					/*This condition is to deal with the case when a user has not verified
-                    the otp before expiry.*/
-				    if(err.code=="ER_DUP_ENTRY"){
-				    	//User details are there in the db but the phone nubmer of the user is not verified yet.
-				    	connection.query('select otp_verified from users where email=?;',user.email,function(err,result){
-				    		if(result[0].otp_verified==0){
-				    			//The result this function would be constants.QUERY_SUCCESS.msg.
-                                cb(null,constants.QUERY_SUCCESS.msg);
-				    		}
-				    		else{
-				    			//Since an error occured the final callback will be executed.
-                                cb(err);
-				    		}
-				    	});
-				    }
-				}
-         			
-         		else
-         			//The result corresponding to one would be constants.QUERY_SUCCESS.msg.
-         			cb(null,constants.QUERY_SUCCESS.msg);
+  
+  connection.query('INSERT INTO users SET ?;',user,function(err,results){
+  var msg;
+  if(err){		
+        
+		if(err.code=="ER_DUP_ENTRY"){
 		
-        		
+		 	connection.query('select otp_verified from users where email=?;',user.email,function(err,result){
+		 		if(result[0].otp_verified==0)
+		 		{
+		 			
+		  			msg="OTP not verified";
+		 		}
 
-			})
-      	},
-      	/*This is the second function and it is dependent on the first function as we want no otp to be generated
-      	before inserting the details of the user and confirming that he does not has its mobile number already verified.        
-         */
-		two:["one",function(arg,cb){
-            //Function to generate a 4 digit OTP.
-         	var otp=randomize('0000');
-         	//Expiry time of OTP in minutes. 
-			sendOtp.setOtpExpiry('5');
-			//Sending the OTP to the user.
-			sendOtp.send(user.phone,"Verification",otp, function (err) {//Retry Sending otp -To Be Done 
-				if(err)
-					cb(err);
-				else
-					//The result of this function will be the otp.
-         			cb(null,otp);
-				
-				})
-		}],
-		/*This is the third and the last function and it is dependent on function one and two i.e. details will be inserted 
-		otp will be generated and then the value of the otp will be stored in db.*/
-		three:["one","two",function(arg,cb){
-					connection.query('update users SET otp=? where email=?;',[arg.two,user.email],function(err,results){
-                      if(err)
-                      	cb(err);
-                      else
-         			    cb(null,constants.QUERY_SUCCESS.msg);
-                                              
-                    })}]
-				}
-                    
-        ,
-        //Final callback.
-        function(err,result){
-         if(err){
-           //Faliure status and msg.
-           res.json({
+                
+                else
+                {
+                	msg="This email is already registered with us.";               	
+                }
+             
+                res.json({
+			    status:constants.SUCCESS.status,
+			    message:constants.SUCCESS.msg,
+			    data:msg,
+
+		    });  
+
+                    	
+			});
+	    }
+	    else{
+          
+	    	res.json({
 			status:constants.FALIURE.status,
 			message:constants.FALIURE.msg,
-			data:err,
-		   });
-           
-           }
-           else{
-           //Success status and msg.
-           res.json({
-			status:constants.SUCCESS.status,
-			message:constants.SUCCESS.msg,
-		   //result will be a json containing the function name and there corresponding results.
-			data:result,
-		});
-       }
+			data:err,});  
+	    }
+		}
+         			
+  else{
+      msg="User details inserted successfully.";
+       res.json({
+			    status:constants.SUCCESS.status,
+			    message:constants.SUCCESS.msg,
+			    data:msg,
+		    }); 
 
-      });
+	  } 
+
+       			
+		
+    });
+
+}
+
+
+module.exports.generate_otp=function(req,res){
+	var msg;
+	var user={		
+		email:req.body.email,
+		phone:req.body.phone,
+	};	
+	    
+      	    
+			//If user has already verified phone no TBD
+			var otp=randomize('0000');
+			sendOtp.setOtpExpiry('30');
+			sendOtp.send(user.phone,"Verification",otp, function (err) {//Retry Sending otp -To Be Done 
+				if(err || user.phone==undefined)
+					msg="OTP not sent!"+err;
+				else
+         			msg="OTP sent successfully!"
+
+         	   res.json({status:constants.SUCCESS.status,
+			   message:constants.SUCCESS.msg,
+			   data:msg});
+				
+				});
+     
+
+
+
       }
 				
 
 
 			
 	
-//Verifying OTP
-module.exports.verify_otp=function(req,res){
 
+module.exports.verify_otp=function(req,res){
+    var msg;
 	var user={
 		email:req.body.email,
 		phone:req.body.phone,
 	    otp:req.body.otp,
 	    	};	
-    	//Function to verify the OTP.
+    	
     	sendOtp.verify(user.phone,user.otp, function (err, data) {
- 		
- 		if(data.type == 'success') 
+  		if(data.type == 'success') 
  		{
- 			//Updation of is_verified column in db if the OTP has been successfully verified. 
+ 			
  			connection.query('update users SET otp_verified=1 where email=?;',[user.email],function(err,results){
-            if(err){
-            	//In case of updation query err throw err rare to occur. 
-            	throw(err);
-            }
-            else{
-            	//Success status and msg.
-         			   res.json({
-			              status:constants.SUCCESS.status,
-			              message:constants.SUCCESS.msg,
-			              });
-                                              
-                 }
-            });
+            msg=constants.SUCCESS.msg;                                  
+            res.json({
+			status:constants.SUCCESS.status,
+			message:constants.SUCCESS.msg,
+		    data:msg
+		    });
 
-
- 	    }
- 	    //For case when user has entered the wrong otp or when the OTP is no longer valid.
-  		if(data.type == 'error'){
-  			
+        });
+        }
+  		
+  		if(data.type == 'error')
+  		{
+  			msg="Otp verification failed!"; 
   			res.json({
 			status:constants.FALIURE.status,
 			message:constants.FALIURE.msg,
-		
-		   });
-		   }
-		});
+		    data:msg
+		    });			
 		}
+
+
+
+		});
+        
+    }
+
+
+module.exports.generate_email=function(req,res){
+var msg;
+
+//If user has already verified email no TBD
+async.series({
+         one : function(callback){
+         rand=Math.floor((Math.random() * 100) + 54);
+         console.log(rand);
+         link="http://"+req.get('host')+"/verify?id="+rand;
+         mailOptions={
+             to : req.query.email,
+             subject : "Please confirm your Email account",
+             html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>" 
+            }
+         smtpTransport.sendMail(mailOptions, function(err, response){
+         if(err){
+            msg=err;
+            
+         }else{	 
+         	msg="Email sent successfully!";
+         	callback(null);
+          }});
+     },
+    two : function(callback){
+   	connection.query('update users SET email_link=? where email=?;',[rand,req.query.email],function(err,results){
+		callback(null);
+	});
+   }},
+
+function(err){
+	if(err){
+		msg=err;
+		res.json({status:constants.FALIURE.status,
+		message:constants.FALIURE.msg,
+		data:msg});
+	}
+	else{
+		res.json({status:constants.SUCCESS.status,
+		message:constants.SUCCESS.msg,
+		data:msg});
+	}
+});
+
+
+
+}
+
+module.exports.verify_email=function(req,res){
+var msg;
+async.waterfall([function(callback){
+connection.query('select email_link from users where email=?',[req.query.email],function(err,result){
+if(err)
+callback(err);
+else
+callback(null,result[0].email_link);
+});
+
+},
+
+function(result,callback){ 
+
+if(req.query.rand==result){
+connection.query('update users set email_verified=1  where email=?',[req.query.email],function(err,result){
+if(err)
+  callback(err);
+else
+  callback(null); 
+
+});}
+
+}],
+
+function(err){
+	if(err){
+		res.json({status:constants.FALIURE.status,
+		message:constants.FALIURE.msg,
+		data:err});
+	}
+	else{
+        msg="Email verified successfully!";
+	    res.json({status:constants.SUCCESS.status,
+		message:constants.SUCCESS.msg,
+		data:msg});
+       }
+   });
+}
+
+
+	
 
